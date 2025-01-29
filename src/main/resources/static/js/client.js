b3d390cca5c2e7358abb54fc87daad8ef0e6e2e2
Alexandra Varga <alexandravarga2000@gmail.com>
2025-01-29 20:02:13 +0200

    Setup CRUD - People
    Signed-off-by: Alexandra Varga <alexandravarga2000@gmail.com>

--- src/main/resources/static/js/client.js
+++ src/main/resources/static/js/client.js
@@ -1,10 +1,6 @@
-/**
- *
- */
-
 $('document').ready(function() {
 
-	$('.table .btn-primary').on('click',function(event){
+	$('.table #editButton').on('click',function(event){
 		event.preventDefault();
 		var href= $(this).attr('href');
 		$.get(href, function(client, status){
@@ -23,37 +19,23 @@ $('document').ready(function() {
 		$('#editModal').modal();
 	});
 
-	/**
-	 *
-	 */
-
-	$('document').ready(function() {
-
-		$('.table .btn-primary').on('click',function(event){
-			event.preventDefault();
-			var href= $(this).attr('href');
-			$.get(href, function(client, status){
-				$('#txtAddressEdit').val(client.address);
-				$('#txtCityEdit').val(client.city);
-				$('#ddlCountryEdit').val(client.countryid);
-				$('#txtDetailsEdit').val(client.details);
-				$('#txtEmailEdit').val(client.email);
-				$('#txtIdEdit').val(client.id);
-				$('#txtMobileEdit').val(client.mobile);
-				$('#txtNameEdit').val(client.name);
-				$('#txtPhoneEdit').val(client.phone);
-				$('#ddlStateEdit').val(client.stateid);
-				$('#txtWebsiteEdit').val(client.website);
-			});
-			$('#editModal').modal();
+	$('.table #detailsButton').on('click',function(event){
+		event.preventDefault();
+		var href= $(this).attr('href');
+		$.get(href, function(client, status){
+			$('#detailstxtAddressEdit').val(client.address);
+			$('#detailstxtCityEdit').val(client.city);
+			$('#detailsddlCountryEdit').val(client.countryid);
+			$('#detailstxtDetailsEdit').val(client.details);
+			$('#detailstxtEmailEdit').val(client.email);
+			$('#detailstxtIdEdit').val(client.id);
+			$('#detailstxtMobileEdit').val(client.mobile);
+			$('#detailstxtNameEdit').val(client.name);
+			$('#detailstxtPhoneEdit').val(client.phone);
+			$('#detailsddlStateEdit').val(client.stateid);
+			$('#detailstxtWebsiteEdit').val(client.website);
 		});
-
-		$('.table #deleteButton').on('click',function(event) {
-			event.preventDefault();
-			var href = $(this).attr('href');
-			$('#deleteModal #delRef').attr('href', href);
-			$('#deleteModal').modal();
-		});
+		$('#detailsModal').modal();
 	});
 
 	$('.table #deleteButton').on('click',function(event) {
